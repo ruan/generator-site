56f275bced3dea0b363c98ed35c44324c90c77fa
Ruan Candido <ruan@cappen.com>
2015-03-06 17:12:04 -0300

    no message

--- app/index.js
+++ app/index.js
@@ -1,61 +1,48 @@
 'use strict';
-var path = require('path');
 var util = require('util');
+var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 
-var CappenSiteGenerator = module.exports = function CappenSiteGenerator(args, options, config) {
-    yeoman.generators.Base.apply(this, arguments);
-    this.on('end', function () {
-        this.installDependencies({ skipInstall: options['skip-install'] });
-    });
-    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
+var CappenSiteGenerator = yeoman.generators.Base.extend({
+    promptUser: function() {
+        var done = this.async();
+        var prompts = [{
+            name: 'appName',
+            message: 'Qual é o nome do site?'
+        }];
+        this.prompt(prompts, function (props) {
+            this.appName = props.appName;
+            done();
+        }.bind(this));
+    },
+    scaffoldFolders: function(){
+        this.mkdir("app");
+        this.mkdir("app/classes");
+        this.mkdir("app/fonts");
+        this.mkdir("app/img");
+        this.mkdir("app/paginas/home");
+        this.mkdir("app/scripts/vendor");
+        this.mkdir("app/scss");
+    },
+    copyMainFiles: function(){
+        this.copy("_gitignore", ".gitignore");
+        this.copy("_gruntfile.js", "Gruntfile.js");
+        this.copy("_htaccess", "app/.htaccess");
+        this.copy("_index.php", "app/index.php");
+        this.copy("_home.php", "app/paginas/home/home.php");
+        this.copy("_App.php", "app/classes/App.php");
+        this.copy("_main.js", "app/scripts/main.php");
+        this.copy("_main.scss", "app/scss/main.scss");
 
-util.inherits(CappenSiteGenerator, yeoman.generators.NamedBase);
+        var context = {
+            site_name: this.appName
+        };
 
-CappenSiteGenerator.prototype.askFor = function askFor() {
-    var done = this.async();
+        this.template("_index.php", "app/index.php", context);
+        this.template("_bower.json", "bower.json", context);
+        this.template("_package.json", "package.json", context);
+    }
+});
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-        'Welcome to the neat ' + chalk.red('Site Generator') + ' generator!'
-    ));
-
-    var prompts = [{
-        name: 'siteName',
-        message: 'Qual o nome do site?'
-    }];
-
-    this.prompt(prompts, function (props) {
-        this.siteName = props.siteName;
-        done();
-    }.bind(this));
-}
-CappenSiteGenerator.prototype.app = function app() {
-    this.mkdir("app");
-    this.mkdir("app/classes");
-    this.mkdir("app/fonts");
-    this.mkdir("app/img");
-    this.mkdir("app/paginas/home");
-    this.mkdir("app/scripts/vendor");
-    this.mkdir("app/scss");
-};
-CappenSiteGenerator.prototype.projectfiles = function projectfiles() {
-    this.copy("_gitignore", ".gitignore");
-    this.copy("_gruntfile.js", "Gruntfile.js");
-    this.copy("_htaccess", "app/.htaccess");
-    this.copy("_home.php", "app/paginas/home/home.php");
-    this.copy("_App.php", "app/classes/App.php");
-    this.copy("_main.js", "app/scripts/main.php");
-    this.copy("_main.scss", "app/scss/main.scss");
-
-    var context = {
-        site_name: this.siteName
-    };
-    //console.log(context);
-    this.template("_index.php", "app/index.php", context);
-    this.template("_bower.json", "bower.json", context);
-    this.template("_package.json", "package.json", context);
-};
+module.exports = CappenSiteGenerator;
